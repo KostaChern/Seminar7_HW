9c7450a03eb8b55a328bbc26aef2b95ea00385c8
Konstantin Chern <codahl@yandex.ru>
2023-03-05 16:07:41 +0300

    Task1 solved

---
+++ Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными
+//  вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+int[,] CreateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns]; // Создаем 2-мерный массив
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(10, 100); // задаем параметр переменных i, j
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");  // значение "\t" это табуляция
+        }
+    }
+    System.Console.WriteLine();
+}
+
+int[,] matrix = CreateArray(3, 4); // задаем размер массива
+PrintArray(matrix);
